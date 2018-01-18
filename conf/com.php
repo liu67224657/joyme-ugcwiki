73df680744cefbaf82439a4ffb1c0af195a01c0e
ericliu <liu67224657@qq.com>
2018-01-18 10:37:42 +0800

    Update com.php

--- conf/com.php
+++ conf/com.php
@@ -3,10 +3,10 @@
 $wgDBname = $wgWikiname . "wiki";
 $wgDBservers = array(
 	array(
-		'host' => "rm-2zem35fsj37eqzu3p.mysql.rds.aliyuncs.com",
+		'host' => "xxxxxxx",
 		'dbname' => $wgWikiname . "wiki",
-		'user' => "wikicr",
-		'password' => "bscIM5yK",
+		'user' => "xxxxx",
+		'password' => "xxxxxxx",
 		'type' => "mysql",
 		'flags' => DBO_DEFAULT,
 		'load' => 0,
@@ -16,14 +16,14 @@ $wgDBservers = array(
 #object cache setting
 $wgObjectCaches['redis'] = array(
     'class' => 'RedisBagOStuff',
-    'servers' => array( 'r-2zef16817404a374.redis.rds.aliyuncs.com:6379' ),
+    'servers' => array( 'xxxx:port' ),
     'persistent' => true,
-    'password'=>'zIGMyY12'
+    'password'=>'xxxxxx'
 );
 $wgMainCacheType = 'redis';
 
 
-$joyme_u_key = 'as__-d(*^(';
+$joyme_u_key = 'xxxxxxx';
 
 
 //七牛云存储配置
@@ -45,8 +45,8 @@ Log::config(Log::ERROR);
 //squid缓存
 $wgUseSquid = true;
 $wgSquidServers = array(
-	'10.172.82.206',
-	'10.51.113.180',
+	'xxxxxxx',
+	'xxxxxxx',
 );
 
 $wgForcedRawSMaxage = 300;
@@ -55,8 +55,8 @@ $wgJobRunRate = 0;
 
 //私信服务
 $wgUserBoardWebSocketHost = '0.0.0.0';
-$wgUserBoardWebSocketPort = '9501';
-$wgUserBoardWebSocketUrl = 'ws://60.205.90.33:'.$wgUserBoardWebSocketPort;
+$wgUserBoardWebSocketPort = 'xxxxx';
+$wgUserBoardWebSocketUrl = 'ws://xx.xx.xx.xx:'.$wgUserBoardWebSocketPort;
 $wgUserBoardWebSocketConfig = array(
 	'max_conn' => 1024,
 	'reactor_num' => 4, //reactor thread num
